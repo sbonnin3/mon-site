38abb8265918179c454a66aac5298aa612dca68d
sbonnin3 <simonbonnin95@gmail.com>
2024-03-18 23:13:16 +0100

    test

---
+++ auth.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginButton = document.getElementById('login-button');
+    loginButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        const email = document.getElementById('user-email').value;
+        const password = document.getElementById('user-password').value;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Connexion réussie
+            document.getElementById('login-message').textContent = `Bienvenue, ${userCredential.user.email}!`;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            document.getElementById('login-message').textContent = `Erreur: ${errorMessage}`;
+        });
+    });
+});
