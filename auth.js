b65817fcabc74d16fb44fda40736b4572508d561
sbonnin3 <simonbonnin95@gmail.com>
2024-03-18 23:19:31 +0100

    test

--- auth.js
+++ auth.js
@@ -1,3 +1,4 @@
+// Assurez-vous que ce script est enregistré en tant que auth.js dans le même répertoire que votre index.html.
 document.addEventListener('DOMContentLoaded', function () {
     const loginButton = document.getElementById('login-button');
     loginButton.addEventListener('click', function(e) {
